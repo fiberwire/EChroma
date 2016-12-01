381ced6ee1051577d9b52a656313ca4e792f16dd
fiberwire <andrew.ramos92@gmail.com>
2016-11-30 22:58:11 -0500

    Got random genome requests working
    Though it probably won't be very useful. I should be calculating phenotypes and sending those instead of genotypes

--- EChroma/Program.cs
+++ EChroma/Program.cs
@@ -1,20 +1,35 @@
-﻿using EChroma.models;
+﻿using Anna;
+using EChroma.models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Linq;
+using Newtonsoft.Json;
 
 namespace EChroma {
     class Program {
         static void Main(string[] args) {
-            var parameters = new ChromaParameters {
-                paths = 20,
-                pathProps = 2
-            };
-            var sequence = Genome.RandomSequence(parameters.paths * parameters.pathProps);
+            ChromaParameters parameters;
 
-            var genome = new Genome(sequence, parameters);
+            using (var requests = new HttpServer("http://*:8000/")) {
+                requests.POST("genome/random")
+                    .Subscribe(r => {
+                        r.Request.GetBody()
+                            .Subscribe(body => {
+                                parameters = JsonConvert.DeserializeObject<ChromaParameters>(body);
+                                var genome = Genome.RandomGenome(parameters.randomLength, parameters);
+                                r.Respond(201, new Dictionary<string, string> {
+                                    {"genome", genome.toJson() }
+                                });
+                            });
+                    });
+                Console.ReadLine();
+            }
+
+
         }
     }
 }
